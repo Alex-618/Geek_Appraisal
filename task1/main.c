b5d61ba888bb3e55619a38b45090e8ecf3e5079a
Your Name <1468289152@qq.com>
2022-10-25 11:43:32 +0800

    Modify1

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
-	return 0;
+int main(int argc, char *argv[]) 
+{
+ double y,h,i,a;
+ h=0.001;
+ scanf("%lf",&a);
+ y=a;
+ printf("(x=%lf,y=%lf)\n",0,y);
+ for(i=0.001;;i=i+0.001)
+ {
+ 	if(y-(2-h)/(2+h)*y<=0.00001)
+ 	{
+ 		printf("(x=%lf,y=%lf)\n",i,(2-h)/(2+h)*y);
+ 		break;
+	 }
+ 	y=(2-h)/(2+h)*y;
+    printf("(x=%lf,y=%lf)\n",i,y);
+ }
+	
+	return 0; 
 }
 
